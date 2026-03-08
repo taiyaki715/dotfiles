36fa2bd3b798fb11c6082469ae2988813a64b13a
Taiki Ishikawa <taiki-ishikawa@r09.jp>
2026-03-08 13:20:40 +0900

    color themeをcatppuccinに変更

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
